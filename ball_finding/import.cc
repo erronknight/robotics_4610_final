4117c3c937055ca6ed777dde2d7c024c8a71ca09
tkaunzinger <tkaunzinger@comcast.net>
2020-11-23 03:25:29 -0500

    added ball_finding with search.hh and search.cc. This will use basic CV techniques to locate the ball in an image and has commands for having the robot search for the ball both when it can and can not find it in frame. This was all made without knowing what the robot interface actually was, so I would strongly recommend someone else comb through it.

---
+++ ball_finding/import.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "search.hh"
+
+// Just makes sure it can actually make and import this lol
+int main(int _argc, char** _argv) {
+    std::cout << "Import Successful!" << std::endl;
+    return 0;
+}
